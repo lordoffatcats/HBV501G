6adc336c284cfa97f3c3b719e3e72481591e61ad
lordoffatcats <atlielvarson@gmail.com>
2026-09-19 18:03:26 +0000

    JWT and security config

---
+++ src/main/java/org/team5/api/security/SecurityConfig.java
@@ -0,0 +1,29 @@
+package org.team5.api.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    /* This method disables authentication for the /accounts/signin and /accounts endpoints
+    * So someone can create an account. Everything else requires a valid JWT. */
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/accounts/signin", "/accounts").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(Customizer.withDefaults())
+                );
+        return http.build();
+    }
+}
